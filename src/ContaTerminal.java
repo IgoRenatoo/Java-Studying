30eca0fa586c77bdf4fd898251707396837f121c
IgoRenatoo <igor.renato.oliveira@gmail.com>
2024-03-16 17:07:41 -0300

    Finish Project

--- src/ContaTerminal.java
+++ src/ContaTerminal.java
@@ -2,13 +2,29 @@ import java.util.Scanner;
 
 public class ContaTerminal {
 public static void main(String[] args) {
-    try (Scanner dataInfo = new Scanner(System.in)) {
-        System.out.println("Informe um número inteiro!");
-        int numberInt = dataInfo.nextInt();
+    Scanner idInfo = new Scanner(System.in);
+        System.out.println("Informe número do seu ID!");
+        String id = idInfo.next();
 
-        System.out.println("Você informou o número: " + numberInt);
+    Scanner agencyInfo = new Scanner(System.in);
+        System.out.println("Informe sua Agência!");
+        int agency = agencyInfo.nextInt();
 
-        dataInfo.close();
-    }
+    Scanner nameInfo = new Scanner(System.in);
+        System.out.println("Informe seu Nome!");
+        String name = nameInfo.nextLine();
+
+    Scanner balanceInfo = new Scanner(System.in);
+        System.out.println("Informe seu Saldo!");
+        Double balance = balanceInfo.nextDouble();
+        
+
+        System.out.printf("%s obrigado por criar uma conta em nosso banco, sua agência é: %s, sua conta é: %s e seu saldo é %.2f e está disponível para saque!", name,agency,id,balance);
+
+        idInfo.close();
+        agencyInfo.close();
+        nameInfo.close();
+        balanceInfo.close();
+
 }
 }
