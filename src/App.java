1d0374b875725cdebc7d3e9ccc4ef67a3b2ff0d7
IgoRenatoo <igor.renato.oliveira@gmail.com>
2024-03-16 15:34:52 -0300

    Delete App.class

--- src/App.java
+++
@@ -1,7 +0,0 @@
-public class App {
-    public static void main(String[] args) {
-        int number = 10;
-        number = 12;
-        System.out.print(number);
-    }
-}
--- src/ContaTerminal.java
+++ src/ContaTerminal.java
@@ -2,6 +2,13 @@ import java.util.Scanner;
 
 public class ContaTerminal {
 public static void main(String[] args) {
-    System.out.printf("Conta Criada!");
+    try (Scanner dataInfo = new Scanner(System.in)) {
+        System.out.println("Informe um número inteiro!");
+        int numberInt = dataInfo.nextInt();
+
+        System.out.println("Você informou o número: " + numberInt);
+
+        dataInfo.close();
+    }
 }
 }
